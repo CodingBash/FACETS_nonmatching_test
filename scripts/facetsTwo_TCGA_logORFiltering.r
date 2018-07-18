05f27d4715c5e4672ee4200177c27379702f91a3
CodingBash <bbecerr@outlook.com>
2018-07-18 18:10:13 -0400

    Reverted facetsTwo logOR filtering into new file

--- scripts/facetsTwo_TCGA_logORFiltering.r
+++ scripts/facetsTwo_TCGA_logORFiltering.r
@@ -15,6 +15,24 @@ createSnpMat <- function(snpMatrix, normalId, tumorId, skip = 0L, err.thresh = I
 	pileup <- snpMatrix[,c(1,2,3,4,
 				normalCol, normalCol + 1, normalCol + 2, normalCol + 3,
 				tumorCol, tumorCol + 1, tumorCol + 2, tumorCol + 3)]
+	
+	delete_rows <- c()
+	delete_rows.i <- 1
+	for(pileup.i in seq(1, nrow(pileup))){
+		logOR <- log2(pileup[pileup.i, 6] / pileup[pileup.i, 10])
+    		print(logOR)
+		print(pileup[pileup.i,])
+		if(is.nan(logOR)) next # TODO: Should nan be removed? let FACETS decide
+    		if(logOR > 3 || logOR < -3){ # If the FileXR + FileXA is less than coverage requirement
+      			delete_rows[delete_rows.i] <- pileup.i
+      			delete_rows.i <- delete_rows.i + 1
+		}
+    	}
+	print(delete_rows)
+	if(!is.null(delete_rows)){
+		pileup <- pileup[-delete_rows,]
+	}
+
 	colnames(pileup) <- c(colnames(pileup)[1:4], "File1R", "File1A",
                           "File1E", "File1D", "File2R", "File2A", "File2E", "File2D")
 	
@@ -32,20 +50,18 @@ createSnpMat <- function(snpMatrix, normalId, tumorId, skip = 0L, err.thresh = I
 	return(rcmat)
 }
 
-snpMatrix <- as.data.frame(read.csv("filteredSnpTCGATest20G5.csv", stringsAsFactors = FALSE, 
+snpMatrix <- as.data.frame(read.csv("logORFilteredSnpTCGATest20G5.csv", stringsAsFactors = FALSE, 
 	colClasses = rep(c("character", "numeric", "character", "numeric"), c(1, 1, 2, 14 * 4))))
 runFacets <- function(rcmat, tumorId, normalId){
 	print(paste("Running FACETS on tumorId:", tumorId, "and normalID:", normalId))
 	set.seed(3224)
-	try({
-		xx <- preProcSample(rcmat, ndepth=20, ndepthmax=1000)
-		oo <- procSample(xx, cval=150)
-		fit<-emcncf(oo)
-	
-		saveRDS(xx, paste0("facetsG5XX_", tumorId, "_", normalId, ".rds"))
-		saveRDS(oo, paste0("facetsG5OO_", tumorId, "_", normalId, ".rds"))
-		saveRDS(fit, paste0("facetsG5Fit_", tumorId, "_", normalId, ".rds"))
-	}, silent = TRUE)
+	xx <- preProcSample(rcmat, ndepth=20, ndepthmax=1000)
+	oo <- procSample(xx, cval=150)
+	fit<-emcncf(oo)
+
+	saveRDS(xx, paste0("facetsG5XX_", tumorId, "_", normalId, ".rds"))
+	saveRDS(oo, paste0("facetsG5OO_", tumorId, "_", normalId, ".rds"))
+	saveRDS(fit, paste0("facetsG5Fit_", tumorId, "_", normalId, ".rds"))
 }
 
 for(NORM.i in NORM){ # Run for non-matching normal
